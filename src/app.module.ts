f8333e1ebff1547af788ea6715eef767a319daa0
cteheranr <kmilo.teheran@gmail.com>
2024-12-04 15:38:33 -0500

    mi back fin

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,22 @@ import { TurnConfigModule } from './turn-config/turn-config.module';
 import { WebsocketGateway } from './Websockets/websocket.gateway';
 import { TurnModule } from './turn/turn.module';
 import { ModuloModule } from './modulo/modulo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // Ruta del archivo .env (opcional si es .env)
+      isGlobal: true,      // Hace el módulo de configuración disponible globalmente
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'mysql',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     UsersModule,
